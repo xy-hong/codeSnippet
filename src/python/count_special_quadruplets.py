2f031a13976a9265f51cd201d9b5982bbcaf7809
Heo <name.xy.hong@gmail.com>
2021-12-29 21:01:12 +0800

    count special quadruplets 2

--- src/python/count_special_quadruplets.py
+++ src/python/count_special_quadruplets.py
@@ -11,3 +11,19 @@ class Solution:
                             result += 1
         
         return result
+
+# 使用字典存储 d
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        result = 0
+        length = len(nums)
+        counts = [0] * 1000
+        for c in range(length-2, 1, -1):
+            d = c + 1
+            counts[nums[d]] += 1
+            for b in range(c-1, 0, -1):
+                for a in range(b-1, -1, -1):
+                    if counts[ nums[a] + nums[b] +nums[c] ] > 0:
+                        result += counts[nums[a] + nums[b] +nums[c]]
+        
+        return result
