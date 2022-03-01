bcce1445e14fa8d36e74f4b2f05c0a3134126037
Heo <name.xy.hong@gmail.com>
2022-03-01 20:30:22 +0800

    reverse string

---
+++ src/python/double_pointer/reverse_string.py
@@ -0,0 +1,14 @@
+# leetcode 344
+# https://leetcode-cn.com/problems/reverse-string/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
