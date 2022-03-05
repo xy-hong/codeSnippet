fccdb80475d3d46f9e9be27ada8cd0814696bf72
Heo <name.xy.hong@gmail.com>
2022-03-05 19:56:04 +0800

    flood fill

---
+++ src/python/graph_search/flood_fill.py
@@ -0,0 +1,36 @@
+# leetcode 733
+# https://leetcode-cn.com/problems/flood-fill/
+
+# breadth first search 
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        init = image[sr][sc]
+
+        if init == newColor:
+            return image
+        
+        max_row = len(image)
+        max_col = len(image[0])
+
+        q = []
+        q.append({"row": sr, "col": sc})
+
+        while len(q) > 0:
+            item = q.pop()
+            row = item['row']
+            col = item['col']
+            image[row][col] = newColor
+
+            if row - 1 >= 0 and image[row-1][col] == init:
+                q.append({"row": row - 1, "col": col})
+
+            if row + 1 < max_row and image[row+1][col] == init:
+                q.append({"row": row + 1, "col": col})
+            
+            if col - 1 >= 0 and image[row][col-1] == init:
+                q.append({"row": row, "col": col - 1})
+            
+            if col + 1 < max_col and image[row][col+1] == init:
+                q.append({"row": row, "col": col+1})
+        
+        return image
