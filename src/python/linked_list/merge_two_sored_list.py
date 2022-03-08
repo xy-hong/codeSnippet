d3adb82fa986cf859ee353c0421b2af5b969927f
Heo <name.xy.hong@gmail.com>
2022-03-08 19:16:55 +0800

    merge two sored list

--- src/python/linked_list/merge_two_sored_list.py
+++ src/python/linked_list/merge_two_sored_list.py
@@ -4,6 +4,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# 双指针
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 is None:
@@ -31,7 +33,23 @@ class Solution:
         
         return result.next       
 
-                
+# 递归
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        
+        if list2 is None:
+            return list1
+
+        next1 = list1.next
+        next2 = list2.next
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(next1, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, next2)
+            return list2             
 
 
 
