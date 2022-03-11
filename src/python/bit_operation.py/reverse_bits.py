c605d9d9a5196b982fb6271fc83a9c15189cf9c6
Heo <name.xy.hong@gmail.com>
2022-03-11 21:28:33 +0800

    reverse bits

---
+++ src/python/bit_operation.py/reverse_bits.py
@@ -0,0 +1,11 @@
+# leetcode 190
+# https://leetcode-cn.com/problems/reverse-bits
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for i in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        return res
