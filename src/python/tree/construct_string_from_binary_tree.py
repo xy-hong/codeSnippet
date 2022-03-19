d7872134beb1790b4edd06cc32653b46ce9199ce
Heo <name.xy.hong@gmail.com>
2022-03-19 12:12:37 +0800

    construct string from binary tree

---
+++ src/python/tree/construct_string_from_binary_tree.py
@@ -0,0 +1,22 @@
+# leetcode 606
+# https://leetcode-cn.com/problems/construct-string-from-binary-tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ""
+        
+        result = str(root.val)
+        if root.left or root.right:
+            result += "(" + self.tree2str(root.left) +")"
+        
+        if root.right:
+            result += "(" + self.tree2str(root.right) + ")"
+
+        return result
